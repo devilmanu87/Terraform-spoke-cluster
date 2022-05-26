4f88c2e88aeac28f04aa5748639a6f52ad6de9d0
Suman Bera <suman.bera0902@gmail.com>
2022-05-26 21:52:28 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+## Install Terraform
+
+choco install terraform
+
+## login to Azure
+az login
+
+## Set Subscription
+az account set --subscription="b62b39b7-71df-44f0-a7fe-ae78cf18c2b8"
+
+## Terraform Init
+terraform Init
